3d551334843ff3375bffe9d5e23bc11cb3d95d13
Eoghan Gallagher <eoghan.gallagher@gmail.com>
2017-11-15 22:09:24 +0000

    Added Pipe Portal

---
+++ mario 2d/Assets/HorizontalPortal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalPortal : MonoBehaviour 
+{
+
+	void OnTriggerEnter2D( Collider2D other ) 
+	{
+        if( other.name.Equals( "Player" ) )
+		{
+			StartCoroutine( "Teleport" );
+		}
+    }
+
+
+	private IEnumerator Teleport()
+	{
+		yield return new WaitForSeconds( 2.0f );
+
+		Debug.Log( "Teleporting" );
+
+	}
+
+	
+	
+}
